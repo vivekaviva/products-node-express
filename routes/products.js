d3a5ea90cac673600107c687b2025bbfdb65814a
vivekaviva <vivekaloganathan37@gmail.com>
2024-09-06 11:27:52 +0530

    With post method

--- routes/products.js
+++ routes/products.js
@@ -12,7 +12,7 @@ router.get("/api/products", (req, res, next) => {
     return { id, name, image, price };
   });
 
-  res.status(200).json({ products: newProducts });
+  res.status(200).json({ sucess: true, status: 200, data: newProducts });
   //res.send(newProducts);
 });
 
@@ -34,7 +34,7 @@ router.get("/api/products/:productID", (req, res) => {
   // const { id, name, image, price } = singleProduct;
   // return res.json({ id, name, image, price });
 
-  return res.json(singleProduct);
+  return res.json({ sucess: true, status: 200, data: singleProduct });
 });
 
 //Review with id
@@ -43,6 +43,7 @@ router.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
   console.log("ids", productID, reviewID);
 
   const product = products.find((product) => product.id === Number(productID));
+
   if (!product) {
     return res.status(404).send("Product Does not exist");
   }
@@ -55,7 +56,7 @@ router.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
     return res.status(404).send("Review Does not exist");
   }
 
-  return res.json(review);
+  return res.json({ sucess: true, status: 200, data: review });
 });
 
 //Review
@@ -74,18 +75,28 @@ router.get("/api/products/reviews/:productID", (req, res) => {
   }
 
   const { reviews } = singleProductReview;
-  return res.json({ reviews });
+
+  // if (reviews.length < 1) {
+  //   return res.status(200).json({ sucess: true, data: [] });
+  // }
+
+  return res.json({ sucess: true, status: 200, data: reviews });
 });
 
 //Filter
+//http://localhost:8080/api/filter?search=39.95
 router.get("/api/filter", (req, res) => {
+  //search ---> al
   const { search } = req.query;
 
   let sortedProducts = [...products];
 
   if (search) {
     sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
+      return product.price.toString().startsWith(search);
+
+      // return product.price.startsWith(search);
+      //return product.name.startsWith(search);
       // return product.name.matchAll(search);
       // return product.name.match(search);
       // return product.name.includes(search);
@@ -95,13 +106,39 @@ router.get("/api/filter", (req, res) => {
   if (sortedProducts.length < 1) {
     return res.status(200).json({ sucess: true, data: [] });
   }
-  res.status(200).json(sortedProducts);
+  res.status(200).json({ sucess: true, data: sortedProducts });
 });
 
 //limit
+//http://localhost:8080/api/limit?limit=3
 router.get("/api/limit", (req, res) => {
   const { limit } = req.query;
   let sortedProducts = [...products];
+
+  if (limit) {
+    //slice(1,3)
+    sortedProducts = sortedProducts.slice(1, Number(limit));
+  }
+
+  if (sortedProducts.length < 1) {
+    return res.status(200).json({ sucess: true, data: [] });
+  }
+
+  res.status(200).json({ sucess: true, data: sortedProducts });
+});
+
+//Limit and Search
+//http://localhost:8080/api/query?limit=3&search=al
+router.get("/api/query", (req, res) => {
+  const { search, limit } = req.query;
+
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
 
   if (limit) {
     sortedProducts = sortedProducts.slice(0, Number(limit));
@@ -111,7 +148,24 @@ router.get("/api/limit", (req, res) => {
     return res.status(200).json({ sucess: true, data: [] });
   }
 
-  res.status(200).json(sortedProducts);
+  return res.status(200).json({ sucess: true, data: sortedProducts });
+});
+
+/**
+ * create products api
+ * Method - post
+ */
+router.post("/api/products", (req, res) => {
+  const { id, name, image, price, desc, reviews } = req.body;
+
+  if (!id || !name || !image || !price || !desc) {
+    return res.status(500).json({ error: "Please provide all the datas" });
+  }
+
+  const newProduct = { id, name, image, price, desc, reviews: reviews || [] };
+  products.push(newProduct);
+
+  res.status(200).json({ sucess: true, data: newProduct });
 });
 
 module.exports = router;
